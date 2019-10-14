0fe57ddaba59fa2d9cf6c4fc018cc30447d73f35
hajczek <i.hajczewska@gmail.com>
2019-10-14 11:07:29 +0200

    Add files to application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <textarea id="editor"></textarea>
+      <div id="preview"></div>
+    </div>
+  );
+}
+
+export default App;
