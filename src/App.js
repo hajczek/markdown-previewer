e160791423f791932dc60d1a574db7d8aca3455e
hajczek <i.hajczewska@gmail.com>
2019-10-17 16:34:47 +0200

    Added functionality of display content from textarea in div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <textarea id="editor"></textarea>
-      <div id="preview"></div>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textareaValue: ''
+    }
+  }
+
+  updateTextareaValue = (e) => {
+    this.setState({
+      textareaValue: e.target.value
+    });
+  }
+
+  render(){
+    let { textareaValue } = this.state;
+    return (
+      <div className="App">
+        <textarea id="editor" value={this.state.textareaValue} onChange={this.updateTextareaValue}></textarea>
+        <div id="preview">{this.state.textareaValue}</div>
+      </div>
+    )
+  } 
 }
 
 export default App;
